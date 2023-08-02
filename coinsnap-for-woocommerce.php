2e553208ddf0f69bb91683c7cd1b1e501be744ba
adagiomedia <59640052+adagiomedia@users.noreply.github.com>
2023-08-02 12:51:33 +0200

    Add files via upload

---
+++ coinsnap-for-woocommerce.php
@@ -0,0 +1,332 @@
+<?php
+/**
+ * Plugin Name:     Coinsnap For Woocommerce Vers. 1 (2023-07)
+ * Plugin URI:      https://wordpress.org/plugins/coinsnap-for-woocommerce/
+ * Description:     With Coinsnap payment processing, you can accept Bitcoin and Lightning payments on your website or online store. You do not need your own Lightning Node or other technical requirements.
+ * Author:          Coinsnap
+ * Author URI:      https://coinsnap.io/
+ * Text Domain:     coinsnap-for-woocommerce
+ * Domain Path:     /languages
+ * Version:         1.0
+ * Requires PHP:    7.4
+ * Tested up to:    6.2
+ * Requires at least: 5.2
+ * WC requires at least: 6.0
+ * WC tested up to: 7.5
+ * License:         GPL2
+ * License URI:     https://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * Network:         true
+ * Update URI:      https://coinsnap.io/plugins/coinsnap-for-woocommerce/update/
+ */
+
+use Coinsnap\WC\Admin\Notice;
+use Coinsnap\WC\Gateway\DefaultGateway;
+use Coinsnap\WC\Helper\SatsMode;
+use Coinsnap\WC\Helper\CoinsnapApiHelper;
+use Coinsnap\WC\Helper\Logger;
+
+defined( 'ABSPATH' ) || exit();
+define( 'SERVER_PHP_VERSION', '7.4' );
+define( 'COINSNAP_VERSION', '1.0' );
+define( 'COINSNAP_VERSION_KEY', 'coinsnap_version' );
+define( 'COINSNAP_PLUGIN_FILE_PATH', plugin_dir_path( __FILE__ ) );
+define( 'COINSNAP_PLUGIN_URL', plugin_dir_url(__FILE__ ) );
+define( 'COINSNAP_PLUGIN_ID', 'coinsnap-for-woocommerce' );
+define( 'COINSNAP_SERVER_URL', 'https://app.coinsnap.io' ); // /api/v1/websites/
+
+class CoinsnapWCPlugin {
+    
+    private static $instance;
+
+    public function __construct() {
+	$this->includes();
+
+	add_action('woocommerce_thankyou_coinsnap', [$this, 'orderStatusThankYouPage'], 10, 1);
+		//add_action( 'wp_ajax_coinsnap_modal_checkout', [$this, 'processAjaxModalCheckout'] );
+		//add_action( 'wp_ajax_nopriv_coinsnap_modal_checkout', [$this, 'processAjaxModalCheckout'] );
+
+		// Run the updates.
+		//\Coinsnap\WC\Helper\UpdateManager::processUpdates();
+
+	if (is_admin()) {
+            // Register our custom global settings page.
+            add_filter(
+                'woocommerce_get_settings_pages',
+                function ($settings) {
+                    $settings[] = new \Coinsnap\WC\Admin\GlobalSettings();
+                    return $settings;
+                }
+            );
+            add_action( 'wp_ajax_handle_ajax_api_url', [$this, 'processAjaxApiUrl'] );
+
+            $this->dependenciesNotification();
+            //$this->legacyPluginNotification();
+            $this->notConfiguredNotification();
+	}
+    }
+
+    public function includes(): void {
+	
+        $autoloader = COINSNAP_PLUGIN_FILE_PATH . 'vendor/autoload.php';
+	if (file_exists($autoloader)) {
+            // @noinspection PhpIncludeInspection 
+            require_once $autoloader;
+	}
+
+	// Make sure WP internal functions are available.
+	if (!function_exists('is_plugin_active') ) {
+            include_once ABSPATH . 'wp-admin/includes/plugin.php';
+	}
+
+	// Setup other dependencies.
+        // Make SAT / Sats as currency available.        
+	if (get_option('coinsnap_sats_mode') === 'yes') {
+            SatsMode::instance();
+	}
+    }
+
+    public static function initPaymentGateways($gateways): array {
+// Default gateway covers all payment methods available on Coinsnap Payment Gateway.
+	$gateways[] = DefaultGateway::class;
+	return $gateways;
+    }
+    
+    public static function enqueueScripts(): void {
+        wp_register_style('coinsnap_payment', plugins_url('assets/css/coinsnap-style.css',__FILE__ ));
+        wp_enqueue_style('coinsnap_payment');
+    }
+
+//  Checks if the plugin is not configured yet method shows notice & link to the config page on admin dashboard
+    public function notConfiguredNotification(): void {
+        if (!CoinsnapApiHelper::getConfig()){
+            $message = sprintf(
+                esc_html__('Plugin not configured yet, please %1$sconfigure the plugin here%2$s','coinsnap-for-woocommerce'),
+		'<a href="' . esc_url(admin_url('admin.php?page=wc-settings&tab=coinsnap_settings')) . '">','</a>'
+            );
+            Notice::addNotice('error', $message);
+        }
+    }
+
+//  Checks if PHP version is too low or WooCommerce is not installed or CURL is not available and displays notice on admin dashboard
+    public function dependenciesNotification() {
+        // Check PHP version.
+	if ( version_compare( PHP_VERSION, SERVER_PHP_VERSION, '<' ) ) {
+            $versionMessage = sprintf( __( 'Your PHP version is %s but Coinsnap Payment plugin requires version '.SERVER_PHP_VERSION.'+.', 'coinsnap-for-woocommerce' ), PHP_VERSION );
+            Notice::addNotice('error', $versionMessage);
+	}
+
+	// Check if WooCommerce is installed.
+	if ( ! is_plugin_active('woocommerce/woocommerce.php') ) {
+            $wcMessage = __('WooCommerce seems to be not installed. Make sure you do before you activate Coinsnap payment gateway.', 'coinsnap-for-woocommerce');
+            Notice::addNotice('error', $wcMessage);
+	}
+
+	// Check if PHP cURL is available.
+	if ( ! function_exists('curl_init') ) {
+            $curlMessage = __('The PHP cURL extension is not installed. Make sure it is available otherwise this plugin will not work.', 'coinsnap-for-woocommerce');
+            Notice::addNotice('error', $curlMessage);
+	}
+    }
+
+//  Handles the AJAX callback from the GlobalSettings form.
+    public function processAjaxApiUrl() {
+        $nonce = $_POST['apiNonce'];
+		if ( ! wp_verify_nonce( $nonce, 'coinsnap-api-url-nonce' ) ) {
+			wp_die('Unauthorized!', '', ['response' => 401]);
+		}
+
+		if ( current_user_can( 'manage_options' ) ) {
+			$host = filter_var($_POST['host'], FILTER_VALIDATE_URL);
+
+			if ($host === false || (substr( $host, 0, 7 ) !== "http://" && substr( $host, 0, 8 ) !== "https://")) {
+				wp_send_json_error("Error validating Coinsnap Server URL.");
+			}
+
+			$permissions = array_merge(CoinsnapApiAuthorization::REQUIRED_PERMISSIONS, CoinsnapApiAuthorization::OPTIONAL_PERMISSIONS);
+
+			try {
+				// Create the redirect url to Coinsnap instance.
+				$url = \Coinsnap\Client\ApiKey::getAuthorizeUrl(
+					$host,
+					$permissions,
+					'WooCommerce',
+					true,
+					true,
+					home_url('?coinsnap-settings-callback'),
+					null
+				);
+
+				// Store the host to options before we leave the site.
+				update_option('coinsnap_url', $host);
+
+				// Return the redirect url.
+				wp_send_json_success(['url' => $url]);
+			} catch (\Throwable $e) {
+				Logger::debug('Error fetching redirect url from BTCPay Server.');
+			}
+		}
+
+        wp_send_json_error("Error processing Ajax request.");
+    }
+
+	/**
+	 * Handles the AJAX callback from the Payment Request on the checkout page.
+	
+	public function processAjaxModalCheckout() {
+
+		Logger::debug('Entering ' . __METHOD__);
+
+		$nonce = $_POST['apiNonce'];
+		if ( ! wp_verify_nonce( $nonce, 'btcpay-nonce' ) ) {
+			wp_die('Unauthorized!', '', ['response' => 401]);
+		}
+
+		if ( get_option('coinsnap_modal_checkout') !== 'yes' ) {
+			wp_die('Modal checkout mode not enabled.', '', ['response' => 400]);
+		}
+
+		wc_maybe_define_constant( 'WOOCOMMERCE_CHECKOUT', true );
+
+		try {
+			WC()->checkout()->process_checkout();
+		} catch (\Throwable $e) {
+			Logger::debug('Error processing modal checkout ajax callback: ' . $e->getMessage());
+		}
+	}*/
+
+    public static function orderStatusThankYouPage($order_id){
+	if (!$order = wc_get_order($order_id)) {
+            return;
+	}
+
+	$title = _x('Payment Status', 'coinsnap-for-woocommerce');
+
+        $orderData = $order->get_data();
+        $status = $orderData['status'];
+
+            switch ($status){
+                case 'on-hold':
+                    $statusDesc = _x('Waiting for payment settlement', 'coinsnap-for-woocommerce');
+                    break;
+                case 'processing':
+                    $statusDesc = _x('Payment processing', 'coinsnap-for-woocommerce');
+                    break;
+		case 'completed':
+                    $statusDesc = _x('Payment settled', 'coinsnap-for-woocommerce');
+                    break;
+		case 'failed':
+                    $statusDesc = _x('Payment failed', 'coinsnap-for-woocommerce');
+                    break;
+		default:
+                    $statusDesc = _x(ucfirst($status), 'coinsnap-for-woocommerce');
+                    break;
+            }
+
+            echo "<section class='woocommerce-order-payment-status'>
+		    <h2 class='woocommerce-order-payment-status-title'>{$title}</h2>
+		    <p><strong>{$statusDesc}</strong></p>
+		</section>";
+    }
+
+//  Gets the main plugin loader instance and ensures only one instance can be loaded.
+    public static function instance(): \CoinsnapWCPlugin {
+        if ( null === self::$instance ) self::$instance = new self();
+	return self::$instance;
+    }
+}
+
+//  Start everything up.
+function init_coinsnap_payment() {
+    \CoinsnapWCPlugin::instance();
+}
+
+//  Bootstrap stuff on init.
+add_action('init', function() {
+    // Adding textdomain and translation support.
+    load_plugin_textdomain('coinsnap-for-woocommerce', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/');
+    // Setting up and handling custom endpoint for api key redirect from Coinsnap Server.
+    add_rewrite_endpoint('coinsnap-settings-callback', EP_ROOT);
+    // Flush rewrite rules only once after activation.
+    if ( ! get_option('coinsnap_permalinks_flushed') ) {
+	flush_rewrite_rules(false);
+	update_option('coinsnap_permalinks_flushed', 1);
+    }
+});
+
+// Action links on plugin overview.
+add_filter( 'plugin_action_links_coinsnap-for-woocommerce/coinsnap-for-woocommerce.php', function ($links){
+    // Settings link.
+    $settings_url = esc_url( add_query_arg(['page' => 'wc-settings','tab' => 'coinsnap_settings'],get_admin_url() . 'admin.php') );
+    $settings_link = "<a href='$settings_url'>" . __( 'Settings', 'coinsnap-for-woocommerce' ) . '</a>';
+    $logs_link = "<a target='_blank' href='" . Logger::getLogFileUrl() . "'>" . __('Debug log', 'coinsnap-for-woocommerce') . "</a>";
+    $docs_link = "<a target='_blank' href='". esc_url('https://coinsnap.io') . "'>" . __('Docs', 'coinsnap-for-woocommerce') . "</a>";
+    $support_link = "<a target='_blank' href='". esc_url('https://coinsnap.io') . "'>" . __('Support Chat', 'coinsnap-for-woocommerce') . "</a>";
+
+    array_unshift($links,$settings_link,$logs_link,$docs_link,$support_link);
+    return $links;
+});
+
+
+// To be able to use the endpoint without appended url segments we need to do this.
+add_filter('request', function($vars) {
+    if (isset($vars['coinsnap-settings-callback'])) {
+        $vars['coinsnap-settings-callback'] = true;
+    }
+    return $vars;
+});
+
+// Adding template redirect handling for coinsnap-settings-callback.
+add_action( 'template_redirect', function() {
+	global $wp_query;
+
+	// Only continue on a coinsnap-settings-callback request.
+	if (! isset( $wp_query->query_vars['coinsnap-settings-callback'] ) ) {
+		return;
+	}
+
+	$coinsnapSettingsUrl = admin_url('admin.php?page=wc-settings&tab=coinsnap_settings');
+
+	$rawData = file_get_contents('php://input');
+	$data = json_decode( $rawData, true );
+
+	// Seems data does get submitted with url-encoded payload, so parse $_POST here.
+	if (!empty($_POST)) {
+		$data['apiKey'] = sanitize_html_class($_POST['apiKey'] ?? null);
+		if (is_array($_POST['permissions'])) {
+			foreach ($_POST['permissions'] as $key => $value) {
+				$data['permissions'][$key] = sanitize_text_field($_POST['permissions'][$key] ?? null);
+			}
+		}
+	}
+
+	if (isset($data['apiKey']) && isset($data['permissions'])) {
+		$apiData = new \Coinsnap\WC\Helper\CoinsnapApiAuthorization($data);
+		if ($apiData->hasSingleStore() && $apiData->hasRequiredPermissions()) {
+			update_option('coinsnap_api_key', $apiData->getApiKey());
+			update_option('coinsnap_store_id', $apiData->getStoreID());
+			Notice::addNotice('success', __('Successfully received api key and store id from Coinsnap Server API. Please finish setup by saving this settings form.', 'coinsnap-for-woocommerce'));
+			wp_redirect($coinsnapSettingsUrl);
+		} else {
+			Notice::addNotice('error', __('Please make sure you only select one store on the Coinsnap API authorization page.', 'coinsnap-for-woocommerce'));
+			wp_redirect($coinsnapSettingsUrl);
+		}
+	}
+
+	Notice::addNotice('error', __('Error processing the data from Coinsnap. Please try again.', 'coinsnap-for-woocommerce'));
+	wp_redirect($coinsnapSettingsUrl);
+});
+
+
+
+// Installation routine.
+register_activation_hook( __FILE__, function() {
+	update_option('coinsnap_permalinks_flushed', 0);
+	update_option( COINSNAP_VERSION_KEY, COINSNAP_VERSION );
+});
+
+// Initialize payment gateways and plugin.
+add_filter( 'woocommerce_payment_gateways', [ 'CoinsnapWCPlugin', 'initPaymentGateways' ] );
+add_action( 'plugins_loaded', 'init_coinsnap_payment', 0 );
+add_action( 'wp_enqueue_scripts', [ 'CoinsnapWCPlugin', 'enqueueScripts' ]  );
+
---
+++ readme.txt
@@ -0,0 +1,203 @@
+=== BTCPay for WooCommerce V2 ===
+Contributors: ndeet, kukks, nicolasdorier
+Donate link: https://btcpayserver.org/donate/
+Tags: bitcoin, btcpay, BTCPay Server, btcpayserver, WooCommerce, payment gateway, accept bitcoin, bitcoin plugin, bitcoin payment processor, bitcoin e-commerce, Lightning Network, Litecoin, cryptocurrency
+Requires at least: 5.2
+Tested up to: 6.2
+Requires PHP: 7.4
+Stable tag: 2.1.0
+License: MIT
+License URI: https://github.com/btcpayserver/woocommerce-greenfield-plugin/blob/master/license.txt
+
+BTCPay Server is a free and open-source bitcoin payment processor which allows you to receive payments in Bitcoin and altcoins directly, with no fees, transaction cost or a middleman.
+
+== Description ==
+
+BTCPay Server is a free and open-source cryptocurrency payment processor which allows you to receive payments in Bitcoin and altcoins directly, with no fees, transaction cost or a middleman.
+
+BTCPay Server is a non-custodial invoicing system which eliminates the involvement of a third-party. Payments with BTCPay WooCommerce Plugin go directly to your wallet, which increases the privacy and security. Your private keys are never uploaded to the server. There is no address re-use since each invoice generates a new address deriving from your xPub key.
+
+You can run BTCPay as a self-hosted solution on your own server, or use a third-party host.
+
+The self-hosted solution allows you not only to attach an unlimited number of stores and use the Lightning Network but also become the payment processor for others.
+
+* Direct, peer-to-peer Bitcoin and altcoin payments
+* No transaction fees (other than mining fees by cryptocurrency network itself)
+* No processing fees
+* No middleman
+* No KYC
+* User has complete control over private keys
+* Enhanced privacy (no address re-use, no IP leaks to third parties)
+* Enhanced security
+* Self-hosted
+* SegWit, Taproot support
+* Lightning Network support (LND, c-lightning and Eclair)
+* Altcoin support
+* Attach unlimited stores, process payments for friends
+* Easy-embeddable Payment buttons
+* Point of Sale app
+
+== Installation ==
+
+This plugin requires WooCommerce. Please make sure you have WooCommerce installed.
+
+<img src="https://github.com/btcpayserver/btcpayserver-doc/blob/master/img/BTCPayWooCommerceInfoggraphic.png" alt="Infographic" />
+
+To integrate BTCPay Server into an existing WooCommerce store, follow the steps below or check our official [installation instructions](https://docs.btcpayserver.org/WooCommerce/).
+
+### 1. Deploy BTCPay Server (optional) ###
+
+This step is optional, if you already have a BTCPay Server instance setup you can skip to section 2. below. To launch your BTCPay server, you can self-host it, or use a third party host.
+
+#### 1.1 Self-hosted BTCPay ####
+
+There are various ways to [launch a self-hosted BTCPay](https://github.com/btcpayserver/btcpayserver-doc#deployment). If you do not have technical knowledge, use the [web-wizard method](https://launchbtcpay.lunanode.com) and follow the video below.
+
+https://www.youtube.com/watch?v=NjslXYvp8bk
+
+For the self-hosted solutions, you will have to wait for your node to sync fully before proceeding to step 3.
+
+#### 1.2 Third-party host ####
+
+Those who want to test BTCPay out, or are okay with the limitations of a third-party hosting (dependency and privacy, as well as lack of some features) can use a one of many [third-party hosts](ThirdPartyHosting.md).
+
+The video below shows you how to connect your store to such a host.
+
+https://www.youtube.com/watch?v=IT2K8It3S3o
+
+### 2. Install BTCPay WooCommerce Plugin ###
+
+BTCPay WooCommerce plugin is a bridge between your BTCPay Server (payment processor) and your e-commerce store. No matter if you are using a self-hosted or third-party solution from step 1., the connection process is identical.
+
+You can find detailed installation instructions on our [WooCommerce documentation](https://docs.btcpayserver.org/WooCommerce/).
+
+Here is a quick walk through if you prefer a video:
+
+https://www.youtube.com/watch?v=ULcocDKZ1Mw
+
+###  3. Connecting your wallet ###
+
+No matter if you're using self-hosted or server hosted by a third-party, the process of configuring your wallet is the same.
+
+https://www.youtube.com/watch?v=xX6LyQej0NQ
+
+### 4. Testing the checkout ###
+
+Making a small test-purchase from your own store, will give you a piece of mind. Always make sure that everything is set up correctly before going live. The final video, guides you through the steps of setting a gap limit in your Electrum wallet and testing the checkout process.
+
+https://www.youtube.com/watch?v=Fi3pYpzGmmo
+
+Depending on your business model and store settings, you may want to fine tune [your order statuses](https://docs.btcpayserver.org/WooCommerce/#41-global-settings).
+
+== Frequently Asked Questions ==
+
+You'll find extensive documentation and answers to many of your questions on [BTCPay for WooCommerce V2 docs](https://docs.btcpayserver.org/WooCommerce) and on [BTCPay for WooCommerce integrations FAQ](https://docs.btcpayserver.org/FAQ/Integrations/#woocommerce-faq).
+
+== Screenshots ==
+
+1. The BTCPay Server invoice. Your customers will see this at the checkout. They can pay from their wallet by scanning a QR or copy/pasting it manually into the wallet.
+2. Customizable plugin interface allows store owners to adjust store statuses according to their needs.
+3. Customer will see the pay with Bitcoin button at the checkout.Text can be customized.
+4. Example of successfully paid invoice.
+5. Example of an easy-embeddable HTML donation payment button.
+6. Example of the PoS app you can launch.
+
+== Changelog ==
+= 2.1.0 :: 2023-04-03 =
+* New feature: Modal / Overlay checkout mode (no redirect to BTCPay Server)
+
+= 2.0.0 :: 2023-03-20 =
+* New feature: Add support for refunds.
+
+Note: If you are upgrading from a version < 2.0 and you want to use refunds (via pull payments) you need to create a new API key with the "Create non-approved pull payments" which is available from BTCPay Server version 1.7.6.
+See this link for more information: https://docs.btcpayserver.org/WooCommerce/#create-a-new-api-key
+
+If you do NOT use refunds. You do NOT need to do anything, your existing API key and setup will continue to work as before.
+
+= 1.1.5 :: 2023-03-08 =
+* Fix: fix error when plugins override delete_transient function not returning boolean value
+
+= 1.1.4 :: 2023-01-20 =
+(redo deployment because of broken build pipe)
+* Fix: fixed error on thank you page for separate payment methods.
+* Dev: updating Docker to latest WP and WC versions.
+* Dev: switch Github action for checkout to v3.
+
+= 1.1.3 :: 2023-01-20 =
+* Fix: fixed error on thank you page for separate payment methods.
+* Dev: updating Docker to latest WP and WC versions.
+* Dev: switch Github action for checkout to v3.
+
+= 1.1.2 :: 2022-12-09 =
+* Fix existing invoice check, wrongly marking invoice invalid on some use cases.
+* Add check for cURL PHP extension.
+* Make sure generated gateways exist on filesystem.
+
+= 1.1.1 :: 2022-09-12 =
+* Update missing metadata
+
+= 1.1.0 :: 2022-09-12 =
+* Feature: Sats-Mode, currency SAT for Satoshis/Sats now available.
+* Settings, adding more links to docs.
+
+= 1.0.3 :: 2022-08-17 =
+* New order state: Payment received after invoice has been expired.
+* Order metadata restructure, also list multiple payments separated.
+* Add plugin action links for settings, logs, docs, support.
+* Show notice when BTCPay Server is not fully synched yet.
+* Add BTCPay Server info to debug log.
+* Update Readme with development instructions.
+* Docker: Update to latest WP and WC versions.
+* Pin BTCPay Server PHP library stable version.
+
+= 1.0.2 :: 2022-04-08 =
+* Fix plugin meta docblock version update, pump version once more.
+
+= 1.0.1 :: 2022-04-08 =
+* Fix bug if the custom uploaded payment gateway icon is deleted from filesystem.
+* Added information about Tor proxy for Umbrel and other self-hosted nodes to BTCPay settings page.
+
+= 1.0.0 :: 2022-03-27 =
+* Reflect stability with release 1.0.0.
+* Create a new invoice (and mark the old invalid) if the user uses browser back button and changes the payment method (relevant for separate payment gateway feature).
+* Added plugin loader singleton.
+* Added missing docs link to separate payment gateways feature.
+* Added checkbox to enable/disable gateway from within gateway settings.
+* Updated README.md
+
+= 0.2.5 :: 2022-03-13 =
+*  Load media library and JS only on payment gateway settings page.
+
+= 0.2.4 :: 2022-03-04 =
+* Fix possible problem with CamelCased headers on PHP-FPM and/or Nginx.
+* Do not log hitting the cache on debug log to avoid clutter.
+
+= 0.2.3 :: 2022-02-28 =
+* Adding irrelevant GitHub workflow files to .distignore.
+* Updating installation instructions with new material.
+
+= 0.2.2 :: 2022-02-28 =
+* Fix fatal error, make sure is_plugin_active() is available.
+
+= 0.2.1 :: 2022-02-21 =
+* Replace SVG by PNG logo to avoid scaling it on themes without proper CSS rules for payment gateway icons.
+
+= 0.2.0 :: 2022-02-18 =
+* Fix Cash on delivery, Bank transfer gateways missing after plugin activation.
+
+= 0.1.10 :: 2022-02-15 =
+* Make sure custom endpoint works without nice url enabled.
+* Better description for setting.
+* Update translation strings.
+
+= 0.1.9 :: 2022-02-08 =
+* Make sure custom endpoint works by flushing rewrite rules on plugin activation.
+* Replacing usage of WC_Admin_Settings::addMessage() with our own.
+
+= 0.1.1 :: 2022-01-13 =
+* Admin notice if legacy plugin is installed
+* Admin notice on missing WooCommerce / PHP version lower 7.4
+* Minor changes metadata / readme.txt
+
+= 0.1.0 :: 2022-01-13 =
+* First public release for testing.
