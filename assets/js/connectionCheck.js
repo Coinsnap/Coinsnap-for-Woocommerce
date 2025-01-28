bd291b99b54d7c506f4db5a3a177c814b708ff5a
adagiomedia <adagiomediarus@gmail.com>
2025-01-28 08:58:41 +0100

    SNAP-1170 Jquery code update

--- assets/js/connectionCheck.js
+++ assets/js/connectionCheck.js
@@ -1,15 +1,18 @@
 jQuery(function ($) {
     
+    let connectionCheckElement = '';
+    
     if($('hr.wp-header-end').length){
-        connectionCheckElement = $('hr.wp-header-end');
+        connectionCheckElement = 'hr.wp-header-end';
     }
     else if($('#jp-admin-notices').length){
-        connectionCheckElement = $('#jp-admin-notices');
+        connectionCheckElement = '#jp-admin-notices';
     }
     
     let ajaxurl = '/wp-admin/admin-ajax.php';
     let data = {
-	action: 'coinsnap_connection_handler'
+	action: 'coinsnap_connection_handler',
+        _wpnonce: wc_secret
     };
 
     jQuery.post( ajaxurl, data, function( response ){
@@ -17,11 +20,13 @@ jQuery(function ($) {
         connectionCheckResponse = $.parseJSON(response);
         let resultClass = (connectionCheckResponse.result === true)? 'success' : 'error';
         
-        if(connectionCheckElement){
-            connectionCheckElement.after('<div class="message '+resultClass+' notice" style="margin-top: 10px;"><p>'+ connectionCheckResponse.message +'</p></div>');
+        $connectionCheckMessage = '<div class="message '+resultClass+' notice" style="margin-top: 10px;"><p>'+ connectionCheckResponse.message +'</p></div>';
+        
+        if(connectionCheckElement !== ''){
+            $(connectionCheckElement).after($connectionCheckMessage);
         }
         else {
-            $('#wpbody-content').prepend('<div class="message '+resultClass+' notice" style="margin-top: 10px;"><p>'+ connectionCheckResponse.message +'</p></div>');
+            $('#wpbody-content').prepend($connectionCheckMessage);
         }
         
         if($('#coinsnapConnectionStatus').length){
