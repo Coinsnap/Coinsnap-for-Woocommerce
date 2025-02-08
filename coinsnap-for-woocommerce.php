9e5613043ac15acc56fcae170f9df3ad5b534617
adagiomedia <adagiomediarus@gmail.com>
2025-02-08 19:31:59 +0100

    SNAP-1313 Tested on WC 9.6.1

--- coinsnap-for-woocommerce.php
+++ coinsnap-for-woocommerce.php
@@ -11,7 +11,7 @@
  * Tested up to:    6.7
  * Requires at least: 5.2
  * WC requires at least: 6.0
- * WC tested up to: 9.6.0
+ * WC tested up to: 9.6.1
  * License:         GPL2
  * License URI:     https://www.gnu.org/licenses/gpl-2.0.html
  *
